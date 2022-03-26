98a3537756599e57b11fbeb405c0dc2899ecc533
Thom Hammond <thomas_hammond@berkeley.edu>
2022-03-25 18:29:08 -0700

    Working on adding conversion from geolocation to city name

--- processkml.py
+++ processkml.py
@@ -1,5 +1,26 @@
+from audioop import add
 import os
 from fastkml import kml
+from geopy.extra.rate_limiter import RateLimiter
+from geopy.geocoders import Nominatim
+
+def getCity(geometry):
+    geolocator = Nominatim(user_agent="barkpark")
+    reverse = RateLimiter(geolocator.reverse, min_delay_seconds=1)
+
+    latitude = str(geometry.y)
+    longitude = str(geometry.x)
+
+    # location = geolocator.reverse(latitude + "," + longitude)
+    location = reverse(latitude + "," + longitude)
+    
+    address = location.raw['address']
+    
+    city = address.get('city', '')
+    if (not city):
+        city = address.get('town', '')
+
+    return city
 
 dirname = os.path.abspath('.')
 
@@ -25,7 +46,22 @@ for placemark in placemarks:
     dogpark = {
         "name": placemark.name,
         "description": placemark.description,
-        "geometry": placemark.geometry
+        "geometry": placemark.geometry,
+        "city": getCity(placemark.geometry)
     }
+
     dogparks.append(dogpark)
+
+# geometry.x == longitude
+# geometry.y == latitude
+
+# e.g. print longitude
+# print(dogparks[0].get("geometry").x)
+
+# for dogpark in dogparks:
+#     print(getCity(dogpark.get('geometry')))
+
+print("city: " + dogparks[0].get("city"))
+
+
 
